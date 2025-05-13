6f663bef2acfa69acd88ecd6d5955c1eaf4f70a1
Pedro Morales <neitan.morales@gmail.com>
2025-05-13 00:41:22 -0600

    Iniciamos fuerte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AlumnosComponent } from './pages/learn-flow/private/alumnos/alumnos.component';
+import { DashboardComponent } from './pages/learn-flow/private/dashboard/dashboard.component';
+import { MateriasComponent } from './pages/learn-flow/private/materias/materias.component';
+import { MainComponent } from './pages/learn-flow/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'learn-flow/main', component: MainComponent },
+  { path: 'learn-flow/dashboard', component: DashboardComponent },
+  { path: 'learn-flow/admin/alumnos', component: AlumnosComponent },
+  { path: 'learn-flow/admin/materias', component: MateriasComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainComponent } from './pages/learn-flow/main/main.component';
+import { DashboardComponent } from './pages/learn-flow/private/dashboard/dashboard.component';
+import { AlumnosComponent } from './pages/learn-flow/private/alumnos/alumnos.component';
+import { MateriasComponent } from './pages/learn-flow/private/materias/materias.component';
+import { UsuariosComponent } from './pages/learn-flow/private/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    MainComponent,
+    DashboardComponent,
+    AlumnosComponent,
+    MateriasComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
